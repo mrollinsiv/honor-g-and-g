f6f9efc5c71927d167e2e0c45e6afaf75fa0f567
Markham F Rollins IV <mrollinsiv@gmail.com>
2018-03-29 09:25:16 -0400

    Added sequelize to handle cache from just giving API, insta pics and races. Updated templates to use new logic.

--- app.js
+++ app.js
@@ -4,6 +4,36 @@ const Router = require('koa-router');
 const nunjucks = require('koa-nunjucks-async');
 const serve = require('koa-static');
 const config = require('./config/config');
+const dbConfig = require('./config/db');
+const request = require('request-promise');
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(dbConfig.database, dbConfig.user, dbConfig.password, {
+  host: dbConfig.host,
+  dialect: dbConfig.adapter,
+  logging: false
+});
+
+const Data = sequelize.define('data', {
+  id:{
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  key:{
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  value: {
+    type: Sequelize.JSONB
+  }
+}, {
+  timestamps: true,
+  freezeTableName: true,
+  tableName: 'data',
+  underscored: true
+});
+
+sequelize.sync();
 
 //and initialize it with
 const app = new Koa();
@@ -14,7 +44,6 @@ app.use(serve('./web'));
 // Send config data to the FE
 app.use(async (ctx,next) => {
   ctx.state.globals = config.public;
-  ctx.state.instaPics = require('./data/instagram').images;
   await next();
 });
 
@@ -28,9 +57,76 @@ app.use(nunjucks('views', nunjucksOptions));
 
 
 router.get('/', async ctx => {
+  // Get fundraising data
+  let fundraisingData = await Data.findOne({where: {key: 'fundraising_data'}});
+  if (!fundraisingData) {
+    fundraisingData = await Data.create({key: 'fundraising_data'});
+  }
+  if (!fundraisingData.value || fundraisingData.updated_at < Date.now() - (24 * 3600 * 1000)) {
+    console.log('update');
+    // Load data from JustGiving API
+    let donationOptions = {
+      uri: config.private.justGiving.uri + 'fundraising/pages/' + config.private.justGiving.shortname + '/donations',
+      headers: {
+        'Content-type': 'application/json',
+        'x-api-key': config.private.justGiving.apiKey
+      },
+      json: true
+    };
+
+    let fundraiserOptions = {
+      uri: config.private.justGiving.uri + 'fundraising/pages/' + config.private.justGiving.shortname,
+      headers: {
+        'Content-type': 'application/json',
+        'x-api-key': config.private.justGiving.apiKey
+      },
+      json: true
+    };
+
+    let donations = {};
+    let fundraiser = {};
+    try {
+      donations = await request(donationOptions);
+      fundraiser = await request(fundraiserOptions);
+
+      // Loop over data and convert GBP to USD
+      for (donation in donations) {
+        if (donation.donorLocalCurrencyCode == 'GBP') {
+          donation.usdAmount = parseFloat(donation.amount) * 1.41;
+        }
+        else {
+          donation.usdAmount = donation.amount;
+        }
+      }
+      fundraisingData.update({value: {donations: donations.donations.slice(0, 10), totalRaised: fundraiser.grandTotalRaisedExcludingGiftAid || 0}});
+    } catch(error) {
+      if (!fundraisingData.value) {
+        // could not load
+        donations = {donations: []};
+        fundraiser = {};
+      }
+    }
+  }
+
+  // Get race data
+  let races = await Data.findOne({where: {key: 'races'}});
+  if (!races) {
+    races = await Data.create({key: 'races'});
+  }
+
+  // Get instagram pics
+  let instaPics = await Data.findOne({where: {key: 'insta_pics'}});
+
   await ctx.render('index', {
-    message: 'Hello World!'
-  })
+    donations: fundraisingData.value.donations,
+    totalRaised: parseInt(fundraisingData.value.totalRaised).toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).split('.')[0],
+    races: races.value,
+    totalMiles: races.value.reduce((a, b) => +a + +b.miles, 0),
+    instaPics: instaPics.value
+  });
 });
 
 //and we'll set up 2 routes, for our index and about me pages
