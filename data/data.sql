5f818a2cb1759da9f14534e1ac676f7b87c48ef5
Markham F Rollins IV <mrollinsiv@gmail.com>
2018-05-18 01:14:11 -0400

    Added auto increment on ID

--- data/data.sql
+++ data/data.sql
@@ -6,6 +6,18 @@ CREATE TABLE data (
     updated_at timestamp with time zone NOT NULL
 );
 
+CREATE SEQUENCE data_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER SEQUENCE data_id_seq OWNED BY data.id;
+ALTER TABLE ONLY data ALTER COLUMN id SET DEFAULT nextval('data_id_seq'::regclass);
+ALTER TABLE ONLY data
+    ADD CONSTRAINT data_pkey PRIMARY KEY (id);
+
 INSERT INTO "data"("id","key","value","created_at","updated_at")
 VALUES
 (9,E'races',E'[]',NOW(),NOW()),
